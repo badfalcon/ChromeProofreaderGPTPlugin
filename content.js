fa980d861aa51daab3cc11bb17abc8d45f61bbaf
badfalcon <badfalcon610@gmail.com>
2024-10-12 00:56:50 +0900

    fix: split the window create process to a function

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ function showLoadingOverlay() {
     overlay.style.width = '100%';
     overlay.style.height = '100%';
     overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    overlay.style.zIndex = '10000';
+    overlay.style.zIndex = '10001';
     overlay.style.display = 'flex';
     overlay.style.justifyContent = 'center';
     overlay.style.alignItems = 'center';
@@ -67,6 +67,75 @@ function displayCorrectedTextWithDiff(originalText, correctedText) {
     });
 }
 
+// 校正結果を表示する関数
+function getProofreadWindow() {
+    let proofreadWindow = document.getElementById('proofread-window');
+
+    if (!proofreadWindow) {
+        proofreadWindow = createProofreadWindow();
+    }
+
+    return proofreadWindow;
+}
+
+// 校正結果を表示するウィンドウを作成する関数
+function createProofreadWindow() {
+    const proofreadWindow = document.createElement('div');
+    proofreadWindow.id = 'proofread-window';
+    proofreadWindow.style.position = 'fixed';
+    proofreadWindow.style.bottom = '20px';
+    proofreadWindow.style.right = '20px';
+    proofreadWindow.style.maxWidth = '80%';
+    proofreadWindow.style.minWidth = '100px';
+    proofreadWindow.style.minHeight = '100px';
+    proofreadWindow.style.padding = '10px';
+    proofreadWindow.style.backgroundColor = '#ffffff';
+    proofreadWindow.style.color = '#000000';
+    proofreadWindow.style.border = '1px solid #ccc';
+    proofreadWindow.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    proofreadWindow.style.zIndex = '10000';
+    proofreadWindow.style.maxHeight = '300px';
+    proofreadWindow.style.fontFamily = 'Arial, sans-serif';
+    proofreadWindow.style.fontSize = '14px';
+
+    const header = document.createElement('div');
+    header.style.display = 'flex';
+    header.style.justifyContent = 'space-between';
+    header.style.alignItems = 'center';
+    header.style.paddingBottom = '5px';
+    header.style.borderBottom = '1px solid lightgray';
+
+    const title = document.createElement('h3');
+    title.style.fontWeight = 'bold';
+    title.style.fontSize = '16px';
+    title.textContent = '校正結果:';
+    header.appendChild(title);
+
+    const closeButton = document.createElement('span');
+    closeButton.textContent = '×';
+    closeButton.style.cursor = 'pointer';
+    closeButton.style.fontSize = '18px';
+    closeButton.style.fontWeight = 'bold';
+
+    closeButton.addEventListener('click', () => {
+        proofreadWindow.remove();
+    });
+
+    header.appendChild(closeButton);
+
+    const result = document.createElement('p');
+    result.style.paddingTop = '10px';
+    result.style.maxHeight = '250px';
+    result.style.overflowY = 'auto';
+
+    proofreadWindow.appendChild(header);
+    proofreadWindow.appendChild(result);
+
+    document.body.appendChild(proofreadWindow);
+
+    return proofreadWindow;
+}
+
 // 差分を計算して、変更箇所のみをハイライトして表示する関数
 function displayCorrectedTextWithHighlights(model, originalText, correctedText) {
     const diff = Diff.diffWords(originalText, correctedText);
@@ -84,54 +153,8 @@ function displayCorrectedTextWithHighlights(model, originalText, correctedText) 
     });
 
     // 結果をページに表示
-    let resultDiv = document.getElementById('proofread-result');
-    // 既に結果表示用のdivがあるか確認
-    if (!resultDiv) {
-        resultDiv = document.createElement('div');
-        resultDiv.id = 'proofread-result';
-        resultDiv.style.position = 'fixed';
-        resultDiv.style.bottom = '20px';
-        resultDiv.style.right = '20px';
-        resultDiv.style.width = '300px';
-
-        // 見やすくするためのスタイルを設定
-        resultDiv.style.backgroundColor = '#ffffff';  // 背景色を白に固定
-        resultDiv.style.color = '#000000';  // 文字色を黒に固定
-        resultDiv.style.border = '1px solid #ccc';
-        resultDiv.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
-        resultDiv.style.zIndex = '10001';
-        resultDiv.style.overflowY = 'auto';
-        resultDiv.style.maxHeight = '300px';
-        resultDiv.style.fontFamily = 'Arial, sans-serif'; // 読みやすいフォント
-        resultDiv.style.fontSize = '14px'; // 適切なフォントサイズ
-
-        // 閉じるボタンを作成
-        const closeButton = document.createElement('span');
-        closeButton.textContent = '×';
-        closeButton.style.position = 'absolute';
-        closeButton.style.top = '5px';
-        closeButton.style.right = '10px';
-        closeButton.style.cursor = 'pointer';
-        closeButton.style.fontSize = '18px';
-        closeButton.style.fontWeight = 'bold';
-
-        // 閉じるボタンをdivに追加
-        document.body.appendChild(resultDiv);
-
-        // 既存の内容をクリアして新しい校正結果を表示
-        resultDiv.innerHTML = `<h3 style="margin: 0; padding-bottom: 10px; font-size: 16px;">校正結果(${model}):</h3>
-                         <p style="padding: 10px;">${resultHTML}</p>`;
-
-        // 閉じるボタンのクリックイベント
-        closeButton.addEventListener('click', () => {
-            resultDiv.remove();
-        });
-
-        // 閉じるボタンの追加
-        resultDiv.appendChild(closeButton);
-    } else {
-        resultDiv.querySelector('p').innerHTML = resultHTML;
-    }
+    let proofreadWindow = getProofreadWindow();
+    proofreadWindow.querySelector('p').innerHTML = resultHTML;
 }
 
 // 校正結果を単純に表示する関数
